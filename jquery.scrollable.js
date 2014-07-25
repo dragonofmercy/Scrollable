202619cc9c540e42bee91f4f613ed0d8c7ad03ae
David Zeller <me@zellerda.com>
2014-07-25 15:24:05 +0200

    Some fixes
    Fix mouseWheel over an element
    Fix update of scrollbar on content change
    Add query mouseWheel

--- jquery.scrollable.js
+++ jquery.scrollable.js
@@ -17,6 +17,8 @@
             this.scrollableContainer = this.element.find('.scrollable-container:eq(0)');
             this.scrolling = { x: { container: null, handle: null }, y: { container: null, handle: null } };
             this.updateContainer();
+            this.lastWidth = this.getAreaSize('width');
+            this.lastHeight = this.getAreaSize('height');
             var $this = this;
             $(window).on('resize', function(){
                 $this.updateContainer();
@@ -35,10 +37,18 @@
                     $(this).removeClass('hover');
                 });
             }
+            setInterval(function(){
+                if($this.lastWidth != $this.getAreaSize('width') || $this.lastHeight != $this.getAreaSize('height')){
+                    $this.element.trigger('update');
+                    $this.lastWidth = $this.getAreaSize('width');
+                    $this.lastHeight = $this.getAreaSize('height');
+                }
+            }, 500);
             if(this.options.mouseWheel){
                 this.element.on('mousewheel', function(e){
                     if(e.deltaY != 0 && $this.scrolling.y.handle != null){
                         if($this.getOffset('top', $this.scrolling.y.handle) >= 0){
+                            e.preventDefault();
                             var new_top = $this.getOffset('top', $this.scrolling.y.handle) + (e.deltaY * -1);
                             new_top = (new_top <= 0 ? 0 : new_top);
                             new_top = (new_top + $this.scrolling.y.handle.height() >= $this.scrolling.y.container.height() ? $this.scrolling.y.container.height() - $this.scrolling.y.handle.height() : new_top);
@@ -48,6 +58,7 @@
                     }
                     if(e.deltaX != 0 && $this.scrolling.x.handle != null){
                         if($this.getOffset('left', $this.scrolling.x.handle) >= 0){
+                            e.preventDefault();
                             var new_left = $this.getOffset('left', $this.scrolling.x.handle) + e.deltaX;
                             new_left = (new_left <= 0 ? 0 : new_left);
                             new_left = (new_left + $this.scrolling.x.handle.width() >= $this.scrolling.x.container.width() ? $this.scrolling.x.container.width() - $this.scrolling.x.handle.width() : new_left);
@@ -200,3 +211,12 @@
         autoHide: true
     };
 })(jQuery);
+
+/*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Version: 3.1.12
+ *
+ * Requires: jQuery 1.2.2+
+ */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a:a(jQuery)}(function(a){function b(b){var g=b||window.event,h=i.call(arguments,1),j=0,l=0,m=0,n=0,o=0,p=0;if(b=a.event.fix(g),b.type="mousewheel","detail"in g&&(m=-1*g.detail),"wheelDelta"in g&&(m=g.wheelDelta),"wheelDeltaY"in g&&(m=g.wheelDeltaY),"wheelDeltaX"in g&&(l=-1*g.wheelDeltaX),"axis"in g&&g.axis===g.HORIZONTAL_AXIS&&(l=-1*m,m=0),j=0===m?l:m,"deltaY"in g&&(m=-1*g.deltaY,j=m),"deltaX"in g&&(l=g.deltaX,0===m&&(j=-1*l)),0!==m||0!==l){if(1===g.deltaMode){var q=a.data(this,"mousewheel-line-height");j*=q,m*=q,l*=q}else if(2===g.deltaMode){var r=a.data(this,"mousewheel-page-height");j*=r,m*=r,l*=r}if(n=Math.max(Math.abs(m),Math.abs(l)),(!f||f>n)&&(f=n,d(g,n)&&(f/=40)),d(g,n)&&(j/=40,l/=40,m/=40),j=Math[j>=1?"floor":"ceil"](j/f),l=Math[l>=1?"floor":"ceil"](l/f),m=Math[m>=1?"floor":"ceil"](m/f),k.settings.normalizeOffset&&this.getBoundingClientRect){var s=this.getBoundingClientRect();o=b.clientX-s.left,p=b.clientY-s.top}return b.deltaX=l,b.deltaY=m,b.deltaFactor=f,b.offsetX=o,b.offsetY=p,b.deltaMode=0,h.unshift(b,j,l,m),e&&clearTimeout(e),e=setTimeout(c,200),(a.event.dispatch||a.event.handle).apply(this,h)}}function c(){f=null}function d(a,b){return k.settings.adjustOldDeltas&&"mousewheel"===a.type&&b%120===0}var e,f,g=["wheel","mousewheel","DOMMouseScroll","MozMousePixelScroll"],h="onwheel"in document||document.documentMode>=9?["wheel"]:["mousewheel","DomMouseScroll","MozMousePixelScroll"],i=Array.prototype.slice;if(a.event.fixHooks)for(var j=g.length;j;)a.event.fixHooks[g[--j]]=a.event.mouseHooks;var k=a.event.special.mousewheel={version:"3.1.12",setup:function(){if(this.addEventListener)for(var c=h.length;c;)this.addEventListener(h[--c],b,!1);else this.onmousewheel=b;a.data(this,"mousewheel-line-height",k.getLineHeight(this)),a.data(this,"mousewheel-page-height",k.getPageHeight(this))},teardown:function(){if(this.removeEventListener)for(var c=h.length;c;)this.removeEventListener(h[--c],b,!1);else this.onmousewheel=null;a.removeData(this,"mousewheel-line-height"),a.removeData(this,"mousewheel-page-height")},getLineHeight:function(b){var c=a(b),d=c["offsetParent"in a.fn?"offsetParent":"parent"]();return d.length||(d=a("body")),parseInt(d.css("fontSize"),10)||parseInt(c.css("fontSize"),10)||16},getPageHeight:function(b){return a(b).height()},settings:{adjustOldDeltas:!0,normalizeOffset:!0}};a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})});
